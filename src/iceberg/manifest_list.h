9b27c31611ea89cdb4c36142e08932b736699888
Junwang Zhao <zhjwpku@gmail.com>
2025-05-20 18:38:54 +0800

    When we don't know deleted files count, we assume that there are deleted files.

--- src/iceberg/manifest_list.h
+++ src/iceberg/manifest_list.h
@@ -166,7 +166,7 @@ struct ICEBERG_EXPORT ManifestFile {
   bool has_existing_files() const { return existing_files_count.value_or(1) > 0; }
 
   /// \brief Checks if this manifest file contains entries with DELETED status
-  bool has_deleted_files() const { return deleted_files_count.value_or(-1) > 0; }
+  bool has_deleted_files() const { return deleted_files_count.value_or(1) > 0; }
 
   inline static const SchemaField kManifestPath =
       SchemaField::MakeRequired(500, "manifest_path", std::make_shared<StringType>(),
