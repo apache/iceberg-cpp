0b20283cf26e9f5025e0b9f6af9b7f86a687bc32
xiao.dong <xiao.dong@clickzetta.com>
2025-09-04 19:21:42 +0800

    feat: manifest writer and adapter impl part2
    1 parse v1v2v3 manifest schema in adapter
    2 convert ManifestFile&ManifestEntry into ArrowArray
    3 add e2e case

--- src/iceberg/manifest_adapter.h
+++ src/iceberg/manifest_adapter.h
@@ -22,6 +22,11 @@
 /// \file iceberg/metadata_adapter.h
 /// Base class of adapter for v1v2v3v4 metadata.
 
+#include <map>
+#include <memory>
+#include <unordered_map>
+#include <unordered_set>
+
 #include "iceberg/arrow_c_data.h"
 #include "iceberg/result.h"
 #include "iceberg/type_fwd.h"
@@ -33,13 +38,24 @@ class ICEBERG_EXPORT ManifestAdapter {
  public:
   ManifestAdapter() = default;
   virtual ~ManifestAdapter() = default;
+  virtual Status Init() = 0;
 
-  virtual Status StartAppending() = 0;
-  virtual Result<ArrowArray> FinishAppending() = 0;
+  Status StartAppending();
+  Result<ArrowArray> FinishAppending();
   int64_t size() const { return size_; }
+  virtual std::shared_ptr<Schema> schema() const = 0;
 
  protected:
+  Status AppendField(ArrowArray* arrowArray, int64_t value);
+  Status AppendField(ArrowArray* arrowArray, uint64_t value);
+  Status AppendField(ArrowArray* arrowArray, double value);
+  Status AppendField(ArrowArray* arrowArray, std::string_view value);
+  Status AppendField(ArrowArray* arrowArray, const std::vector<uint8_t>& value);
+
+ protected:
+  bool is_initialized_ = false;
   ArrowArray array_;
+  ArrowSchema schema_;  // converted from manifest_schema_ or manifest_list_schema_
   int64_t size_ = 0;
 };
 
@@ -47,10 +63,48 @@ class ICEBERG_EXPORT ManifestAdapter {
 // append a list of `ManifestEntry`s to an `ArrowArray`.
 class ICEBERG_EXPORT ManifestEntryAdapter : public ManifestAdapter {
  public:
-  ManifestEntryAdapter() = default;
-  ~ManifestEntryAdapter() override = default;
+  explicit ManifestEntryAdapter(std::shared_ptr<Schema> partition_schema,
+                                std::shared_ptr<PartitionSpec> partition_spec)
+      : partition_spec_(std::move(partition_spec)),
+        partition_schema_(std::move(partition_schema)) {}
+  ~ManifestEntryAdapter() override;
 
   virtual Status Append(const ManifestEntry& entry) = 0;
+
+  std::shared_ptr<Schema> schema() const override { return manifest_schema_; }
+
+ protected:
+  virtual std::shared_ptr<StructType> GetManifestEntryStructType();
+  Status InitSchema(const std::unordered_set<int32_t>& fields_ids);
+  Status AppendInternal(const ManifestEntry& entry);
+  Status AppendDataFile(ArrowArray* arrow_array,
+                        const std::shared_ptr<StructType>& data_file_type,
+                        const std::shared_ptr<DataFile>& file);
+  Status AppendPartitions(ArrowArray* arrow_array,
+                          const std::shared_ptr<StructType>& partition_type,
+                          const std::vector<Literal>& partitions);
+  Status AppendList(ArrowArray* arrow_array, const std::vector<int32_t>& list_value);
+  Status AppendList(ArrowArray* arrow_array, const std::vector<int64_t>& list_value);
+  Status AppendMap(ArrowArray* arrow_array, const std::map<int32_t, int64_t>& map_value);
+  Status AppendMap(ArrowArray* arrow_array,
+                   const std::map<int32_t, std::vector<uint8_t>>& map_value);
+
+  virtual Result<std::optional<int64_t>> GetWrappedSequenceNumber(
+      const ManifestEntry& entry);
+  virtual Result<std::optional<std::string>> GetWrappedReferenceDataFile(
+      const std::shared_ptr<DataFile>& file);
+  virtual Result<std::optional<int64_t>> GetWrappedFirstRowId(
+      const std::shared_ptr<DataFile>& file);
+  virtual Result<std::optional<int64_t>> GetWrappedContentOffset(
+      const std::shared_ptr<DataFile>& file);
+  virtual Result<std::optional<int64_t>> GetWrappedContentSizeInBytes(
+      const std::shared_ptr<DataFile>& file);
+
+ protected:
+  std::shared_ptr<PartitionSpec> partition_spec_;
+  std::shared_ptr<Schema> partition_schema_;
+  std::shared_ptr<Schema> manifest_schema_;
+  std::unordered_map<std::string, std::string> metadata_;
 };
 
 // \brief Implemented by different versions with different schemas to
@@ -58,9 +112,26 @@ class ICEBERG_EXPORT ManifestEntryAdapter : public ManifestAdapter {
 class ICEBERG_EXPORT ManifestFileAdapter : public ManifestAdapter {
  public:
   ManifestFileAdapter() = default;
-  ~ManifestFileAdapter() override = default;
+  ~ManifestFileAdapter() override;
 
   virtual Status Append(const ManifestFile& file) = 0;
+
+  std::shared_ptr<Schema> schema() const override { return manifest_list_schema_; }
+
+ protected:
+  Status InitSchema(const std::unordered_set<int32_t>& fields_ids);
+  Status AppendInternal(const ManifestFile& file);
+  Status AppendPartitions(ArrowArray* arrow_array,
+                          const std::shared_ptr<ListType>& partition_type,
+                          const std::vector<PartitionFieldSummary>& partitions);
+
+  virtual Result<int64_t> GetWrappedSequenceNumber(const ManifestFile& file);
+  virtual Result<int64_t> GetWrappedMinSequenceNumber(const ManifestFile& file);
+  virtual Result<std::optional<int64_t>> GetWrappedFirstRowId(const ManifestFile& file);
+
+ protected:
+  std::shared_ptr<Schema> manifest_list_schema_;
+  std::unordered_map<std::string, std::string> metadata_;
 };
 
 }  // namespace iceberg
---
+++ src/iceberg/v1_metadata.cc
@@ -0,0 +1,91 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#include "iceberg/v1_metadata.h"
+
+#include "iceberg/manifest_entry.h"
+#include "iceberg/manifest_list.h"
+#include "iceberg/partition_spec.h"
+#include "iceberg/schema.h"
+
+namespace iceberg {
+
+Status ManifestEntryAdapterV1::Init() {
+  static std::unordered_set<int32_t> compatible_fields{
+      0, 1, 2, 100, 101, 102, 103, 104, 105, 108, 109, 110, 137, 125, 128, 131, 132, 140,
+  };
+  // TODO(xiao.dong) schema to json
+  metadata_["schema"] = "{}";
+  // TODO(xiao.dong) partition spec to json
+  metadata_["partition-spec"] = "{}";
+  if (partition_spec_ != nullptr) {
+    metadata_["partition-spec-id"] = std::to_string(partition_spec_->spec_id());
+  }
+  metadata_["format-version"] = "1";
+  return InitSchema(compatible_fields);
+}
+
+Status ManifestEntryAdapterV1::Append(const iceberg::ManifestEntry& entry) {
+  return AppendInternal(entry);
+}
+
+std::shared_ptr<StructType> ManifestEntryAdapterV1::GetManifestEntryStructType() {
+  // V1 compatible fields. Official suggestions:
+  // Deprecated. Always write a default in v1. Do not write in v2 or v3.
+  static const SchemaField kBlockSizeInBytes = SchemaField::MakeRequired(
+      105, "block_size_in_bytes", iceberg::int64(), "Block size in bytes");
+  std::shared_ptr<StructType> partition_type = partition_schema_;
+  if (!partition_type) {
+    partition_type = PartitionSpec::Unpartitioned()->schema();
+  }
+  auto datafile_type = std::make_shared<StructType>(std::vector<SchemaField>{
+      DataFile::kFilePath, DataFile::kFileFormat,
+      SchemaField::MakeRequired(102, DataFile::kPartitionField,
+                                std::move(partition_type)),
+      DataFile::kRecordCount, DataFile::kFileSize, kBlockSizeInBytes,
+      DataFile::kColumnSizes, DataFile::kValueCounts, DataFile::kNullValueCounts,
+      DataFile::kNanValueCounts, DataFile::kLowerBounds, DataFile::kUpperBounds,
+      DataFile::kKeyMetadata, DataFile::kSplitOffsets, DataFile::kSortOrderId});
+
+  return std::make_shared<StructType>(
+      std::vector<SchemaField>{ManifestEntry::kStatus, ManifestEntry::kSnapshotId,
+                               SchemaField::MakeRequired(2, ManifestEntry::kDataFileField,
+                                                         std::move(datafile_type))});
+}
+
+Status ManifestFileAdapterV1::Init() {
+  static std::unordered_set<int32_t> compatible_fields{
+      500, 501, 502, 503, 504, 505, 506, 512, 513, 514, 507, 519,
+  };
+  metadata_["snapshot-id"] = std::to_string(snapshot_id_);
+  metadata_["parent-snapshot-id"] = parent_snapshot_id_.has_value()
+                                        ? std::to_string(parent_snapshot_id_.value())
+                                        : "null";
+  metadata_["format-version"] = "1";
+  return InitSchema(compatible_fields);
+}
+
+Status ManifestFileAdapterV1::Append(const iceberg::ManifestFile& file) {
+  if (file.content != ManifestFile::Content::kData) {
+    return InvalidManifestList("Cannot store delete manifests in a v1 table");
+  }
+  return AppendInternal(file);
+}
+
+}  // namespace iceberg
