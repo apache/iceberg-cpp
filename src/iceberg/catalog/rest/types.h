e497ec49127a7d24956e8eadc207774d6eee809e
Li Feiyang <lifeiyang@zju.edu.cn>
2025-10-23 18:16:30 +0800

    fix review

--- src/iceberg/catalog/rest/types.h
+++ src/iceberg/catalog/rest/types.h
@@ -19,6 +19,7 @@
 
 #pragma once
 
+#include <memory>
 #include <optional>
 #include <string>
 #include <unordered_map>
@@ -26,20 +27,58 @@
 
 #include "iceberg/catalog/rest/iceberg_rest_export.h"
 #include "iceberg/table_identifier.h"
-#include "iceberg/table_metadata.h"
+#include "iceberg/type_fwd.h"
+
+/// \file iceberg/catalog/rest/types.h
+/// Request and response types for Iceberg REST Catalog API.
 
 namespace iceberg::rest {
+
+/// \brief Request to create a namespace.
+struct ICEBERG_REST_EXPORT CreateNamespaceRequest {
+  Namespace namespace_;  // required
+  std::unordered_map<std::string, std::string> properties;
+};
+
+/// \brief Update or delete namespace properties request.
+struct ICEBERG_REST_EXPORT UpdateNamespacePropertiesRequest {
+  std::vector<std::string> removals;
+  std::unordered_map<std::string, std::string> updates;
+};
+
+/// \brief Request to create a table.
+struct ICEBERG_REST_EXPORT CreateTableRequest {
+  std::string name;  // required
+  std::string location;
+  std::shared_ptr<Schema> schema;  // required
+  std::shared_ptr<PartitionSpec> partition_spec;
+  std::shared_ptr<SortOrder> write_order;
+  std::optional<bool> stage_create;
+  std::unordered_map<std::string, std::string> properties;
+};
+
+/// \brief Request to register a table.
+struct ICEBERG_REST_EXPORT RegisterTableRequest {
+  std::string name;               // required
+  std::string metadata_location;  // required
+  bool overwrite = false;
+};
+
+/// \brief Request to rename a table.
+struct ICEBERG_REST_EXPORT RenameTableRequest {
+  TableIdentifier source;       // required
+  TableIdentifier destination;  // required
+};
 
 /// \brief An opaque token that allows clients to make use of pagination for list APIs.
 using PageToken = std::string;
 
 /// \brief Result body for table create/load/register APIs.
-/// \details Matches **components/schemas/LoadTableResult** in the REST spec.
 struct ICEBERG_REST_EXPORT LoadTableResult {
   std::optional<std::string> metadata_location;
-  TableMetadata metadata;  // required
+  std::shared_ptr<TableMetadata> metadata;  // required  // required
   std::unordered_map<std::string, std::string> config;
-  // TODO(Li Feiyang): Add std::vector<StorageCredential> storage_credentials;
+  // TODO(Li Feiyang): Add std::shared_ptr<StorageCredential> storage_credential;
 };
 
 /// \brief Alias of LoadTableResult used as the body of CreateTableResponse
@@ -48,32 +87,25 @@ using CreateTableResponse = LoadTableResult;
 /// \brief Alias of LoadTableResult used as the body of LoadTableResponse
 using LoadTableResponse = LoadTableResult;
 
-/// \brief Alias of LoadTableResult used as the body of RegisterTableResponse
-using RegisterTableResponse = LoadTableResult;
-
 /// \brief Response body for listing namespaces.
-/// Contains all namespaces and an optional pagination token.
 struct ICEBERG_REST_EXPORT ListNamespacesResponse {
-  std::optional<PageToken> next_page_token;
+  PageToken next_page_token;
   std::vector<Namespace> namespaces;
 };
 
 /// \brief Response body after creating a namespace.
-/// \details Contains the created namespace and its resolved properties.
 struct ICEBERG_REST_EXPORT CreateNamespaceResponse {
   Namespace namespace_;  // required
   std::unordered_map<std::string, std::string> properties;
 };
 
 /// \brief Response body for loading namespace properties.
-/// \details Contains stored properties, may be null if unsupported by the server.
 struct ICEBERG_REST_EXPORT GetNamespaceResponse {
   Namespace namespace_;  // required
   std::unordered_map<std::string, std::string> properties;
 };
 
 /// \brief Response body after updating namespace properties.
-/// \details Lists keys that were updated, removed, or missing.
 struct ICEBERG_REST_EXPORT UpdateNamespacePropertiesResponse {
   std::vector<std::string> updated;  // required
   std::vector<std::string> removed;  // required
@@ -81,9 +113,8 @@ struct ICEBERG_REST_EXPORT UpdateNamespacePropertiesResponse {
 };
 
 /// \brief Response body for listing tables in a namespace.
-/// \details Contains all table identifiers and an optional pagination token.
 struct ICEBERG_REST_EXPORT ListTablesResponse {
-  std::optional<PageToken> next_page_token;
+  PageToken next_page_token;
   std::vector<TableIdentifier> identifiers;
 };
 
