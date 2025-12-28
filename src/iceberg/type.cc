2388e1562d4cd9faa15aedd00c89d1b8da14a47f
Xinli Shang <shangx@uber.com>
2025-12-28 12:14:31 -0800

    refactor: remove obsolete format string workaround
    Remove workaround for old Clang/libc++ bug where "<{}>" in format
    strings was incorrectly parsed. Modern compilers handle this correctly.
    
    Changes:
    - Simplify ListType::ToString() to use direct format string
    - Simplify MapType::ToString() to use direct format string
    - Remove XXX comments about the workaround
    
    The existing tests will verify this works correctly on current compilers.

--- src/iceberg/type.cc
+++ src/iceberg/type.cc
@@ -151,12 +151,7 @@ ListType::ListType(int32_t field_id, std::shared_ptr<Type> type, bool optional)
 
 TypeId ListType::type_id() const { return kTypeId; }
 std::string ListType::ToString() const {
-  // XXX: work around Clang/libc++: "<{}>" in a format string appears to get
-  // parsed as {<>} or something; split up the format string to avoid that
-  std::string repr = "list<";
-  std::format_to(std::back_inserter(repr), "{}", element_);
-  repr += ">";
-  return repr;
+  return std::format("list<{}>", element_);
 }
 
 std::span<const SchemaField> ListType::fields() const { return {&element_, 1}; }
@@ -213,13 +208,7 @@ const SchemaField& MapType::value() const { return fields_[1]; }
 TypeId MapType::type_id() const { return kTypeId; }
 
 std::string MapType::ToString() const {
-  // XXX: work around Clang/libc++: "<{}>" in a format string appears to get
-  // parsed as {<>} or something; split up the format string to avoid that
-  std::string repr = "map<";
-
-  std::format_to(std::back_inserter(repr), "{}: {}", key(), value());
-  repr += ">";
-  return repr;
+  return std::format("map<{}: {}>", key(), value());
 }
 
 std::span<const SchemaField> MapType::fields() const { return fields_; }
